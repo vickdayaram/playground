7f40d0d95543718428f7070c4fddf6d2cae186ba
vickdayaram <vickdayaram@gmail.com>
2020-07-26 16:42:19 -0400

    draggable set up, now actually implemnt drags

--- packages/play/src/components/Draggable/Draggable.tsx
+++ packages/play/src/components/Draggable/Draggable.tsx
@@ -31,33 +31,43 @@ export const Draggable = () => {
   return (
     <div className={styles.draggable}>
       <div className={styles.controlPanel}>
+        <div className={styles.header}>Control Panel</div>
         <Button onClick={handleReset} type={"primary"}> Reset </Button>
       </div>
-      {dragItems.map(({ name, color, x, y }) => (
-        <div
-          onMouseDown={handleMouseDown}
-          className={styles.dragItem}
-          key={name}
-          style={{
-            backgroundColor: color,
-            left: `${x}px`,
-            right: `${y}px`
-          }}>
-          {name}
-        </div>
-      ))}
+      <div className={styles.draggableWindow} id={"draggableWindow"}>
+        {dragItems.map(({ name, color, x, y }) => (
+          <div
+            onMouseDown={handleMouseDown}
+            className={styles.dragItem}
+            key={name}
+            style={{
+              backgroundColor: color,
+              left: `${x}px`,
+              top: `${y}px`
+            }}>
+            {name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 
 const resetDragItems = (): DragItem[] => {
+
+  const padding = 100;
+
   const createInitialPos = (): Pos => {
-    const wh = window.innerHeight;
-    const ww = window.innerWidth;
-    const y = Math.random() * wh;
-    const x = Math.random() * ww;
-    return { x, y };
+    const dw = document.getElementById('draggableWindow');
+    if (dw) {
+      const wh = dw.clientHeight;
+      const ww = dw.clientWidth;
+      const y = Math.abs((Math.random() * wh) - padding);
+      const x = Math.abs((Math.random() * ww) - padding);
+      return { x, y };
+    }
+    return { x: 0, y: 0 };
   }
 
   return [
